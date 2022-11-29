3df0bd986dd71927575db68676946010194517e4
Rodrigo Chin <rodrigo.chin@outlook.com>
2022-11-29 13:20:55 -0300

    Add state-button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
-import { ZipcodeEntryComponent } from './zipcode-entry/zipcode-entry.component';
-import {LocationService} from "./location.service";
-import { ForecastsListComponent } from './forecasts-list/forecasts-list.component';
-import {WeatherService} from "./weather.service";
 import { CurrentConditionsComponent } from './current-conditions/current-conditions.component';
+import { ForecastsListComponent } from './forecasts-list/forecasts-list.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import {RouterModule} from "@angular/router";
-import {routing} from "./app.routing";
-import {HttpClientModule} from "@angular/common/http";
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { StateButtonComponent } from './state-button/state-button.component';
+import { ZipcodeEntryComponent } from './zipcode-entry/zipcode-entry.component';
+
+import { LocationService } from './location.service';
+import { WeatherService } from './weather.service';
+import { routing } from './app.routing';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ZipcodeEntryComponent,
-    ForecastsListComponent,
     CurrentConditionsComponent,
-    MainPageComponent
+    ForecastsListComponent,
+    MainPageComponent,
+    StateButtonComponent,
+    ZipcodeEntryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule,
     routing,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
---
+++ src/app/state-button/state-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-state-button',
+  templateUrl: './state-button.component.html',
+  styleUrls: ['./state-button.component.css']
+})
+export class StateButtonComponent {
+}
