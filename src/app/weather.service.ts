e5fecba108c18fec5b8c26984f4b2a894b1bb432
Rodrigo Chin <rodrigo.chin@outlook.com>
2022-11-29 13:57:00 -0300

    Change open weater api url for https

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -11,7 +11,7 @@ type Condition = { zip: string, country: string, data: Weather };
 @Injectable()
 export class WeatherService {
 
-  static URL = 'http://api.openweathermap.org/data/2.5';
+  static URL = 'https://api.openweathermap.org/data/2.5';
   static APPID = '5a4b2d457ecbef9eb2a71e480b947604';
   static ICON_URL = 'https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/';
   static bufferSize = 3;
