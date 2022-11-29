f0010e898696f7b32c6256c4f8c579dbb32bc1cc
Rodrigo Chin <rodrigo.chin@outlook.com>
2022-11-29 13:21:31 -0300

    Adjust styling and add error alert

--- src/app/auto-filter/auto-filter.component.ts
+++ src/app/auto-filter/auto-filter.component.ts
@@ -15,16 +15,16 @@ type Option = { label: string; value: string };
   ]
 })
 export class AutoFilterComponent implements ControlValueAccessor {
-  @HostBinding('class') class = 'form-inline';
-  @Input() placeholder: string;
+  @HostBinding('style.display') display = 'inherit';
 
   private _options: Option[];
   @Input() set options(value: string[]) {
     this._options = value.map(v => ({ label: v.toUpperCase(), value: v }));
   };
+  @Input() placeholder: string;
 
   protected active = new Subject<boolean>();
-  protected control = new FormControl();
+  protected control = new FormControl('');
   protected onChange: (value: string) => void;
   protected onTouched: (event: Event) => void;
 
@@ -33,7 +33,7 @@ export class AutoFilterComponent implements ControlValueAccessor {
     this.control.valueChanges.pipe(startWith(''))
   ]).pipe(
     debounceTime(300),
-    map(([active, value]) => active ? value.toUpperCase() : null),
+    map(([active, value]) => active ? value?.toUpperCase() : null),
     map(value => ({ options: this._options.filter(o => o.label.includes(value)), value })),
     map(res => res.options.map(o => ({ value: o.value, html: o.label.replace(res.value, `<strong>${res.value}</strong>`) }))),
     map(options => options.slice(0, 8))
@@ -64,5 +64,6 @@ export class AutoFilterComponent implements ControlValueAccessor {
   writeValue(value: string) {
     const option = this._options.find(o => o.label === value?.toUpperCase());
     if (option) this.control.setValue(option.value);
+    else this.control.reset('');
   }
 }
--- src/app/state-button/state-button.component.ts
+++ src/app/state-button/state-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 type StateType = 'default' | 'done' | 'working';
 
@@ -7,21 +7,22 @@ type StateType = 'default' | 'done' | 'working';
   templateUrl: './state-button.component.html'
 })
 export class StateButtonComponent {
-  state: StateType = 'default';
+  protected state: StateType = 'default';
 
+  @Input() disabled: boolean;
   @Output() onClick = new EventEmitter<undefined>();
-
-  click() {
-    this.state = 'working';
-    this.onClick.emit();
-  }
 
   complete() {
     this.state = 'done';
     setTimeout(() => this.state = 'default', 500);
   }
 
-  getClass() {
+  protected click() {
+    this.state = 'working';
+    this.onClick.emit();
+  }
+
+  protected getClass() {
     switch (this.state) {
       case 'default':
         return 'btn btn-primary';
